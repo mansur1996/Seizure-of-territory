da71b1ace32f3422d18b4573161f86b418618120
MrMansur <mirzayevmn96@gmail.com>
2022-08-31 23:39:02 -0700

    commit

--- app/src/main/java/com/SeizureOfTerritory/fragment/HomeFragment.kt
+++ app/src/main/java/com/SeizureOfTerritory/fragment/HomeFragment.kt
@@ -5,25 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.SeizureOfTerritory.R
-import kotlinx.android.synthetic.main.fragment_home.view.*
+import com.SeizureOfTerritory.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
-    private lateinit var root : View
-
+    private lateinit var binding : FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        root = inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
 
-        root.textView.setOnClickListener {
-            root.findNavController().navigate(R.id.gameFragment)
+        initViews()
+        return binding.root
+    }
+
+    private fun initViews() {
+        binding.tvStart.setOnClickListener {
+            findNavController().navigate(R.id.gameFragment)
         }
-
-        return root
     }
 }
